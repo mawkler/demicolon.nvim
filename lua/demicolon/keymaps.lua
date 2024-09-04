3f2fd86c2036c19ccd8723463408c31cfd945a70
Vinzent <vinzent03@proton.me>
2024-09-04 14:17:39 +0200

    feat: add desc to key maps

--- lua/demicolon/keymaps.lua
+++ lua/demicolon/keymaps.lua
@@ -19,16 +19,16 @@ function M.create_default_horizontal_keymaps()
 end
 
 function M.create_default_diagnostic_keymaps()
-  vim.keymap.set(nxo, ']d', jump.diagnostic_jump({ forward = true }))
-  vim.keymap.set(nxo, '[d', jump.diagnostic_jump({ forward = false }))
-  vim.keymap.set(nxo, ']e', jump.diagnostic_jump({ forward = true, severity = 'ERROR' }))
-  vim.keymap.set(nxo, '[e', jump.diagnostic_jump({ forward = false, severity = 'ERROR' }))
-  vim.keymap.set(nxo, ']w', jump.diagnostic_jump({ forward = true, severity = 'WARN' }))
-  vim.keymap.set(nxo, '[w', jump.diagnostic_jump({ forward = false, severity = 'WARN' }))
-  vim.keymap.set(nxo, ']i', jump.diagnostic_jump({ forward = true, severity = 'INFO' }))
-  vim.keymap.set(nxo, '[i', jump.diagnostic_jump({ forward = false, severity = 'INFO' }))
-  vim.keymap.set(nxo, ']h', jump.diagnostic_jump({ forward = true, severity = 'HINT' }))
-  vim.keymap.set(nxo, '[h', jump.diagnostic_jump({ forward = false, severity = 'HINT' }))
+  vim.keymap.set(nxo, ']d', jump.diagnostic_jump({ forward = true }), {desc = "Next diagnostic"})
+  vim.keymap.set(nxo, '[d', jump.diagnostic_jump({ forward = false }), {desc = "Previous diagnostic"})
+  vim.keymap.set(nxo, ']e', jump.diagnostic_jump({ forward = true, severity = 'ERROR' }), {desc = "Next error"})
+  vim.keymap.set(nxo, '[e', jump.diagnostic_jump({ forward = false, severity = 'ERROR' }), {desc = "Previous error"})
+  vim.keymap.set(nxo, ']w', jump.diagnostic_jump({ forward = true, severity = 'WARN' }), {desc = "Next warning"})
+  vim.keymap.set(nxo, '[w', jump.diagnostic_jump({ forward = false, severity = 'WARN' }), {desc = "Previous warning"})
+  vim.keymap.set(nxo, ']i', jump.diagnostic_jump({ forward = true, severity = 'INFO' }), {desc = "Next info"})
+  vim.keymap.set(nxo, '[i', jump.diagnostic_jump({ forward = false, severity = 'INFO' }), {desc = "Previous info"})
+  vim.keymap.set(nxo, ']h', jump.diagnostic_jump({ forward = true, severity = 'HINT' }), {desc = "Next hint"})
+  vim.keymap.set(nxo, '[h', jump.diagnostic_jump({ forward = false, severity = 'HINT' }), {desc = "Previous hint"})
 end
 
 return M
