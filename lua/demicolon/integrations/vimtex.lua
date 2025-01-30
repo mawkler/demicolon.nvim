cfe47889f2acec55735595ee5e2abcedab15df75
Vinzent <vinzent03@proton.me>
2025-01-30 16:37:42 +0100

    style: use single quotes

--- lua/demicolon/integrations/vimtex.lua
+++ lua/demicolon/integrations/vimtex.lua
@@ -19,10 +19,10 @@ function M.jump(options)
 
       -- Manually store the count and prepend it to the mapping to preserve it
       local count = vim.v.count
-      local count_str = count > 1 and tostring(count) or ""
-      local mapping = count_str .. "<Plug>(vimtex-" .. vimtex_mapping .. ")"
+      local count_str = count > 1 and tostring(count) or ''
+      local mapping = count_str .. '<Plug>(vimtex-' .. vimtex_mapping .. ')'
       local plug_mapping = vim.api.nvim_replace_termcodes(mapping, true, false, true)
-      vim.api.nvim_feedkeys(plug_mapping, "m", false)
+      vim.api.nvim_feedkeys(plug_mapping, 'm', false)
     end, options)
   end
 end
@@ -34,13 +34,13 @@ function M.vimtex_map(key, vimtex_mapping, desc)
   -- Override only if it's a vimtex mapping or not set.
   -- That's roughly the behavior of vimtex as well:
   -- https://github.com/lervag/vimtex/blob/83e331dcad5ce28012e656eea3906b5b897db2ba/autoload/vimtex.vim#L415
-  local existing_lhs = vim.fn.maparg(vimtex_mapping, "nxo")
-  if (existing_lhs ~= '' and existing_lhs:sub(1, 13) ~= "<Plug>(vimtex") then
+  local existing_lhs = vim.fn.maparg(vimtex_mapping, 'nxo')
+  if (existing_lhs ~= '' and existing_lhs:sub(1, 13) ~= '<Plug>(vimtex') then
     return
   end
 
   local vimtex_key = vimtex_mapping:sub(2, 2)
-  local forward = vimtex_mapping:sub(1, 1) == "]"
+  local forward = vimtex_mapping:sub(1, 1) == ']'
 
   local nxo = { 'n', 'x', 'o' }
 
@@ -55,39 +55,39 @@ function M.create_keymaps()
     return
   end
 
-  vim.api.nvim_create_autocmd("FileType", {
+  vim.api.nvim_create_autocmd('FileType', {
     group = vim.api.nvim_create_augroup('demicolon_vimtex_keymap', {}),
-    pattern = "tex",
+    pattern = 'tex',
     callback = function()
-      M.vimtex_map(keymaps.section_start.next, "][", "Next section start")
-      M.vimtex_map(keymaps.section_start.prev, "[[", "Previous section start")
+      M.vimtex_map(keymaps.section_start.next, '][', 'Next section start')
+      M.vimtex_map(keymaps.section_start.prev, '[[', 'Previous section start')
 
-      M.vimtex_map(keymaps.section_end.next, "]]", "Next section end")
-      M.vimtex_map(keymaps.section_end.prev, "[]", "Previous section end")
+      M.vimtex_map(keymaps.section_end.next, ']]', 'Next section end')
+      M.vimtex_map(keymaps.section_end.prev, '[]', 'Previous section end')
 
-      M.vimtex_map(keymaps.frame_start.next, "]r", "Next frame start")
-      M.vimtex_map(keymaps.frame_start.prev, "[r", "Previous frame start")
+      M.vimtex_map(keymaps.frame_start.next, ']r', 'Next frame start')
+      M.vimtex_map(keymaps.frame_start.prev, '[r', 'Previous frame start')
 
-      M.vimtex_map(keymaps.frame_end.next, "]R", "Next frame end")
-      M.vimtex_map(keymaps.frame_end.prev, "[R", "Previous frame end")
+      M.vimtex_map(keymaps.frame_end.next, ']R', 'Next frame end')
+      M.vimtex_map(keymaps.frame_end.prev, '[R', 'Previous frame end')
 
-      M.vimtex_map(keymaps.math_start.next, "]n", "Next math start")
-      M.vimtex_map(keymaps.math_start.prev, "[n", "Previous math start")
+      M.vimtex_map(keymaps.math_start.next, ']n', 'Next math start')
+      M.vimtex_map(keymaps.math_start.prev, '[n', 'Previous math start')
 
-      M.vimtex_map(keymaps.math_end.next, "]N", "Next math end")
-      M.vimtex_map(keymaps.math_end.prev, "[N", "Previous math end")
+      M.vimtex_map(keymaps.math_end.next, ']N', 'Next math end')
+      M.vimtex_map(keymaps.math_end.prev, '[N', 'Previous math end')
 
-      M.vimtex_map(keymaps.comment_start.next, "]/", "Next comment start")
-      M.vimtex_map(keymaps.comment_start.prev, "[/", "Previous comment start")
+      M.vimtex_map(keymaps.comment_start.next, ']/', 'Next comment start')
+      M.vimtex_map(keymaps.comment_start.prev, '[/', 'Previous comment start')
 
-      M.vimtex_map(keymaps.comment_end.next, "]%", "Next comment end")
-      M.vimtex_map(keymaps.comment_end.prev, "[%", "Previous comment end")
+      M.vimtex_map(keymaps.comment_end.next, ']%', 'Next comment end')
+      M.vimtex_map(keymaps.comment_end.prev, '[%', 'Previous comment end')
 
-      M.vimtex_map(keymaps.environment_start.next, "]m", "Next environment start")
-      M.vimtex_map(keymaps.environment_start.prev, "[m", "Previous environment start")
+      M.vimtex_map(keymaps.environment_start.next, ']m', 'Next environment start')
+      M.vimtex_map(keymaps.environment_start.prev, '[m', 'Previous environment start')
 
-      M.vimtex_map(keymaps.environment_end.next, "]M", "Next environment end")
-      M.vimtex_map(keymaps.environment_end.prev, "[M", "Previous environment end")
+      M.vimtex_map(keymaps.environment_end.next, ']M', 'Next environment end')
+      M.vimtex_map(keymaps.environment_end.prev, '[M', 'Previous environment end')
     end,
   })
 end
