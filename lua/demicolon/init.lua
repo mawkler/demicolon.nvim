4bf385c2b8d95431af68357d358b8bb04b5e99aa
Melker Ulander <melker.ulander@pm.me>
2025-04-23 09:04:13 +0200

    feat!: switch to using vim.on_key

--- lua/demicolon/init.lua
+++ lua/demicolon/init.lua
@@ -2,145 +2,75 @@ local keymaps = require('demicolon.keymaps')
 
 local M = {}
 
----@class DemicolonDiagnosticOptions
+---@class demicolon.diagnostic.options
 ---@field float? boolean|vim.diagnostic.Opts.Float Default options passed to diagnostic floating window
 
----@class DemicolonKeymapsOptions
+---@class demicolon.keymaps.options
 ---@field horizontal_motions? boolean Create `t`/`T`/`f`/`F` key mappings
----@field diagnostic_motions? boolean Create ]d/[d, etc. key mappings to jump to diganostics. See demicolon.keymaps.create_default_diagnostic_keymaps.
 ---@field repeat_motions? boolean Create `;` and `,` key mappings
----@field list_motions? boolean Create `]q`/`[q`/`]<C-q>`/`[<C-q>` and `]l`/`[l`/`]<C-l>`/`[<C-l>` quickfix and location list mappings
----@field spell_motions? boolean Create `]s`/`[s` key mappings for jumping to spelling mistakes
----@field fold_motions? boolean Create `]z`/`[z` key mappings for jumping to folds
+---@field custom_keys? table<string> Keys for custom motion keys, not including the `]` prefix. I.e. if you want to add repeat support for the motion `]R`, add `'R'` to `custom_keys`
+---@field native_keys? table<string> | false Native keys, many of them listed under `:help ]`. Set to `false` to disable
 
----@class DemicolonIntegrationKeymaps
----@field next? string
----@field prev? string
-
----@class DemicolonGitsignsOptions
----@field enabled? boolean
----@field keymaps? DemicolonIntegrationKeymaps
-
----@class DemicolonNeotestKeymapOptions
----@field test? DemicolonIntegrationKeymaps
----@field failed_test? DemicolonIntegrationKeymaps
-
----@class DemicolonNeotestOptions
----@field enabled? boolean
----@field keymaps? DemicolonNeotestKeymapOptions
-
----@class DemicolonVimtexKeymapOptions
----@field section_start? DemicolonIntegrationKeymaps
----@field section_end? DemicolonIntegrationKeymaps
----@field frame_start? DemicolonIntegrationKeymaps
----@field frame_end? DemicolonIntegrationKeymaps
----@field math_start? DemicolonIntegrationKeymaps
----@field math_end? DemicolonIntegrationKeymaps
----@field comment_start? DemicolonIntegrationKeymaps
----@field comment_end? DemicolonIntegrationKeymaps
----@field environment_start? DemicolonIntegrationKeymaps
----@field environment_end? DemicolonIntegrationKeymaps
-
----@class DemicolonVimtexOptions
----@field enabled? boolean
----@field keymaps? DemicolonVimtexKeymapOptions
-
----@class DemicolonIntegrationOptions
----@field gitsigns? DemicolonGitsignsOptions Integration with https://github.com/lewis6991/gitsigns.nvim
----@field neotest? DemicolonNeotestOptions Integration with https://github.com/nvim-neotest/neotest
----@field vimtex? DemicolonVimtexOptions Integration with https://github.com/lervag/vimtex
-
----@class DemicolonOptions
----@field diagnostic? DemicolonDiagnosticOptions Diagnostic options
----@field keymaps? DemicolonKeymapsOptions Create default keymaps
----@field integrations? DemicolonIntegrationOptions Integrations with other plugins
+---@class demicolon.options
+---@field diagnostic? demicolon.diagnostic.options Diagnostic options
+---@field keymaps? demicolon.keymaps.options Create default keymaps
 local options = {
-  diagnostic = {
-    float = {},
-  },
   keymaps = {
     horizontal_motions = true,
-    diagnostic_motions = true,
     repeat_motions = true,
-    list_motions = true,
-    spell_motions = true,
-    fold_motions = true,
-  },
-  integrations = {
-    gitsigns = {
-      enabled = true,
-      keymaps = {
-        next = ']c',
-        prev = '[c',
-      },
+    custom_keys = {
+      'R', -- Vimtex frame end
+      'n', -- Vimtex math start
+      'N', -- Vimtex math end
     },
-    neotest = {
-      enabled = true,
-      keymaps = {
-        test = {
-          next = ']t',
-          prev = '[t',
-        },
-        failed_test = {
-          next = ']T',
-          prev = '[T',
-        },
-      },
-    },
-    vimtex = {
-      enabled = true,
-      keymaps = {
-        section_start = {
-          next = ']]',
-          prev = '[[',
-        },
-        section_end = {
-          next = '][',
-          prev = '[]',
-        },
-        frame_start = {
-          next = ']r',
-          prev = '[r',
-        },
-        frame_end = {
-          next = ']R',
-          prev = '[R',
-        },
-        math_start = {
-          next = ']n',
-          prev = '[n',
-        },
-        math_end = {
-          next = ']N',
-          prev = '[N',
-        },
-        comment_start = {
-          next = ']/',
-          prev = '[/',
-        },
-        comment_end = {
-          next = ']*',
-          prev = '[*',
-        },
-        environment_start = {
-          next = ']m',
-          prev = '[m',
-        },
-        environment_end = {
-          next = ']M',
-          prev = '[M',
-        },
-      }
+    native_keys = {
+      "'",
+      '#',
+      ')',
+      '*',
+      '/',
+      '`',
+      '[',
+      ']',
+      '{',
+      '}',
+      'A',
+      'B',
+      'D',
+      'I',
+      'L',
+      'M',
+      'P',
+      'Q',
+      'S',
+      'T',
+      'a',
+      'b',
+      'c',
+      'd',
+      'f',
+      'i',
+      'l',
+      'm',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'z',
+      'CTRL-L',
+      'CTRL-Q',
+      'CTRL-T',
     }
   },
 }
 
----@return DemicolonOptions
+---@return demicolon.options
 function M.get_options()
   return options
 end
 
----@param opts? DemicolonOptions
+---@param opts? demicolon.options
 function M.setup(opts)
   options = vim.tbl_deep_extend('force', options, opts or {})
 
@@ -152,33 +82,14 @@ function M.setup(opts)
     keymaps.create_default_repeat_keymaps()
   end
 
-  if options.keymaps.diagnostic_motions then
-    keymaps.create_default_diagnostic_keymaps()
-  end
+  require('demicolon.deprecation').warn_for_deprecated_options(options)
 
-  if options.keymaps.list_motions then
-    keymaps.create_default_list_keymaps()
-  end
+  vim.list_extend(
+    options.keymaps.custom_keys,
+    options.keymaps.native_keys or {}
+  )
 
-  if options.keymaps.spell_motions then
-    keymaps.create_default_spell_keymaps()
-  end
-
-  if options.keymaps.fold_motions then
-    keymaps.create_default_fold_keymaps()
-  end
-
-  if options.integrations.gitsigns.enabled then
-    require('demicolon.integrations.gitsigns').create_keymaps()
-  end
-
-  if options.integrations.neotest.enabled then
-    require('demicolon.integrations.neotest').create_keymaps()
-  end
-
-  if options.integrations.vimtex.enabled then
-    require('demicolon.integrations.vimtex').create_keymaps()
-  end
+  require('demicolon.jump').listen_for_repetable_bracket_motions(options.keymaps.custom_keys)
 end
 
 return M
--- lua/demicolon/jump.lua
+++ lua/demicolon/jump.lua
@@ -2,15 +2,10 @@ local ts_repeatable_move = require('nvim-treesitter.textobjects.repeatable_move'
 
 local M = {}
 
----@alias DemicolonJumpOpts { forward: boolean }
+---@alias demicolon.jump.opts { forward: boolean }
 
----@param keys string
-local function feedkeys(keys)
-  vim.api.nvim_feedkeys(vim.v.count1 .. keys, 'xn', true)
-end
-
----@param func fun(opts: (table | DemicolonJumpOpts), additional_args?: ...) Repeatable function to be called. It should determine by the `forward` boolean whether to move forward or backward
----@param opts table | DemicolonJumpOpts Options to pass to the function. Make sure to include the `forward` boolean
+---@param func fun(opts: (table | demicolon.jump.opts), additional_args?: ...) Repeatable function to be called. It should determine by the `forward` boolean whether to move forward or backward
+---@param opts table | demicolon.jump.opts Options to pass to the function. Make sure to include the `forward` boolean
 ---@param additional_args? any[]
 function M.repeatably_do(func, opts, additional_args)
   opts = opts or {}
@@ -24,127 +19,108 @@ function M.repeatably_do(func, opts, additional_args)
   func(opts, unpack(additional_args))
 end
 
+---@param key string
+---@param repeatable_bracket_keys table<string>
+---@return boolean
+local function native_repeatable_motion(previous_key, key, repeatable_bracket_keys)
+  local split_trigger = vim.tbl_contains({ ']', '[' }, previous_key)
+      and vim.tbl_contains(repeatable_bracket_keys, key)
+  return split_trigger
+end
+
+--- NOTE: assumes that all repeatable commands are two characters long, for
+--- example `]f`
+---@param typed string
+---@param repeatable_bracket_keys table<string>
+---@return boolean
+local function remapped_repeatable_motion(typed, repeatable_bracket_keys)
+  return vim.tbl_contains(repeatable_bracket_keys, typed:sub(2))
+end
+
+---@param forward boolean
+---@param motion string
+---@return string
+local function motion_from_direction(forward, motion)
+  local prefix = motion:sub(1, 1) -- `]` or `[`
+  assert(prefix == ']' or '[', 'demicolon.nvim: motion does not start with ] or [')
+  local key = motion:sub(2)       -- The rest of the motion
+
+  -- This deals with the special cases where the second character is a
+  -- delimiter, which behave opposite to all other bracket motions
+  local opposite_delimiters = {
+    ['['] = ']',
+    [']'] = '[',
+    ['('] = ')',
+    [')'] = '(',
+    ['{'] = '}',
+    ['}'] = '{',
+  }
+
+  local new_prefix = forward and ']' or '['
+  local new_key = new_prefix ~= prefix
+      and opposite_delimiters[key]
+      -- Fallback
+      or key
+
+  return new_prefix .. new_key
+end
+
+
+---@param string string
+local function has_bracket_prefix(string)
+  local prefix = string:sub(1, 1)
+  return prefix == ']' or '['
+end
+
+---@param repeatable_bracket_keys table<string>
+function M.listen_for_repetable_bracket_motions(repeatable_bracket_keys)
+  local previous_key
+
+  vim.on_key(function(_, typed)
+    -- `keytrans` deals with modifier cases like `]CTRL-Q` for instance
+    typed = vim.fn.keytrans(typed)
+
+    -- For native (non-remapped) commands, `typed` will be split into two
+    -- characters. That's what `previous_key` is for. For commands that have
+    -- been remapped `vim.on_key` will only get called once for the entire
+    -- command, and `typed` will be the entire command.
+
+    local motion
+
+    if native_repeatable_motion(previous_key, typed, repeatable_bracket_keys) then
+      motion = previous_key .. typed
+    elseif
+        has_bracket_prefix(typed)
+        and remapped_repeatable_motion(typed, repeatable_bracket_keys)
+    then
+      motion = typed
+    else
+      -- Not a recognized repeatable command
+      previous_key = typed
+      return
+    end
+
+    if motion then
+      ts_repeatable_move.last_move = {
+        func = function(opts)
+          local new_motion = motion_from_direction(opts.forward, motion)
+          local keys = vim.api.nvim_replace_termcodes(new_motion, true, false, true)
+          vim.api.nvim_feedkeys(keys, 'x', true)
+        end,
+        opts = {},
+        additional_args = {},
+      }
+    end
+
+    previous_key = nil -- Reset previous key on recognized command
+  end)
+end
+
 ---@param key 't' | 'T' | 'f' | 'F'
 ---@return fun(): string
 function M.horizontal_jump(key)
   return function()
     return ts_repeatable_move['builtin_' .. key .. '_expr']()
-  end
-end
-
----@deprecated Use horizontal_jump instead
-function M.horizontal_jump_repeatably(opts)
-  vim.notify(
-    'diagnostic.nvim: `horizontal_jump_repeatably` is deprecated. Use `horizontal_jump` instead.')
-  return M.horizontal_jump(opts)
-end
-
----@class DemicolonDiagnosticJumpOpts: vim.diagnostic.GotoOpts
----@field forward boolean
-
----@param opts DemicolonDiagnosticJumpOpts Note that the `count` field will be overridden by `vim.v.count1`
----@return function
-function M.diagnostic_jump(opts)
-  return function()
-    M.repeatably_do(function(o)
-      o = o or {}
-      local float_opts = require('demicolon').get_options().diagnostic.float
-      o.float = type(float_opts) == 'table' and vim.tbl_extend('force', o, float_opts) or float_opts
-
-      local count = o.forward and 1 or -1
-      o.count = count * vim.v.count1
-
-      if vim.diagnostic.jump then
-        vim.diagnostic.jump(o)
-      else
-        -- Deprecated in favor of `vim.diagnostic.jump` in Neovim 0.11.0
-        if o.count > 0 then
-          vim.diagnostic.goto_next(o)
-        else
-          vim.diagnostic.goto_prev(o)
-        end
-      end
-    end, opts)
-  end
-end
-
----@deprecated Use `diagnostic_jump` instead
-function M.diagnostic_jump_repeatably(opts)
-  vim.notify(
-    'diagnostic.nvim: `diagnostic_jump_repeatably` is deprecated. Use `diagnostic_jump` instead.')
-  return M.diagnostic_jump(opts)
-end
-
----@class DemicolonListJumpOpts
----@field forward boolean If `true`, jump forwards, otherwise jump backwards
----@field file? boolean  If `true`, jump by file (see `:help cnfile`/`:help lnfile`)
-
----@param type 'quickfix' | 'location'
----@param opts DemicolonListJumpOpts
-local function list_jump(type, list, opts)
-  require('demicolon.jump').repeatably_do(function(o)
-    if vim.tbl_isempty(list) then
-      return
-    end
-
-    local direction = o.forward and 'n' or 'p'
-    local prefix = type == 'quickfix' and 'c' or 'l'
-    local file_suffix = opts.file and 'f' or ''
-    local command = prefix .. direction .. file_suffix
-
-    -- Special case for `:help :lne` which is inconsistently named
-    if command == 'ln' then
-      command = 'lne'
-    end
-
-    ---@diagnostic disable-next-line: param-type-mismatch
-    pcall(vim.cmd, vim.v.count1 .. command)
-  end, { forward = opts.forward }, opts)
-end
-
----@param opts DemicolonListJumpOpts
----@return function
-function M.quickfix_list_jump(opts)
-  return function()
-    local list = vim.fn.getqflist()
-    return list_jump('quickfix', list, opts)
-  end
-end
-
----@param opts DemicolonJumpOpts
----@return function
-function M.location_list_jump(opts)
-  return function()
-    local list = vim.fn.getloclist(0)
-    return list_jump('location', list, opts)
-  end
-end
-
----@param opts DemicolonJumpOpts
----@return function
-function M.fold_jump(opts)
-  return function()
-    require('demicolon.jump').repeatably_do(function(o)
-      local direction = o.forward and 'j' or 'k'
-      feedkeys('z' .. direction)
-    end, { forward = opts.forward }, opts)
-  end
-end
-
----@param opts DemicolonJumpOpts
----@return function
-function M.spell_jump(opts)
-  return function()
-    require('demicolon.jump').repeatably_do(function(o)
-      -- `]s`/`[s` only work if `spell` is enabled
-      local spell = vim.wo.spell
-      vim.wo.spell = true
-
-      local direction = o.forward and ']' or '['
-      feedkeys(direction .. 's')
-
-      vim.wo.spell = spell
-    end, { forward = opts.forward }, opts)
   end
 end
 
